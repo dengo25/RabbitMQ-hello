752b8a0e047489d46856ce4d91c99ce0f0bbc52e
dengo25 <dengo25.dev@gmail.com>
2025-10-30 18:29:17 +0900

    [feat] 구성 요소 선언

--- src/main/java/com/example/rabibitmqhello/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabibitmqhello/config/RabbitMQConfig.java
@@ -1,5 +1,10 @@
 package com.example.rabibitmqhello.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -8,4 +13,20 @@ public class RabbitMQConfig {
   public static final String EXCHANGE_NAME = "hello.exchange";
   public static final String QUEUE_NAME = "hello.queue";
   public static final String ROUTING_KEY = "hello.key.#";
+  
+  @Bean
+  public TopicExchange exchange() {
+    return new TopicExchange(EXCHANGE_NAME);
+  }
+  
+  @Bean
+  public Queue queue() {
+    // durable: true(기본값) - 서버가 재시작 되어도 큐가 유지됨
+    return new Queue(QUEUE_NAME);
+  }
+  
+  @Bean
+  public Binding binding(Queue queue, TopicExchange exchange) {
+    return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+  }
 }
