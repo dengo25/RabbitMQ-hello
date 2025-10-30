d3b9f351d7f5b06938f6cc865d1ca68a1c2914f5
dengo25 <dengo25.dev@gmail.com>
2025-10-30 18:47:31 +0900

    [feat] consumer 수정

--- src/main/java/com/example/rabibitmqhello/consumer/HelloConsumer.java
+++ src/main/java/com/example/rabibitmqhello/consumer/HelloConsumer.java
@@ -1,6 +1,7 @@
 package com.example.rabibitmqhello.consumer;
 
 import com.example.rabibitmqhello.config.RabbitMQConfig;
+import com.example.rabibitmqhello.dto.OrderDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class HelloConsumer {
   
   @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME) //이 어노테이션으로 지정된 큐를 수신대기하는 메시지 리스너가 된다
-  public void receiveMessage(String message) {
-    log.info("received message: {}", message);
+  public void receiveMessage(OrderDto order) {
+    log.info("Received Order message: {}", order.toString());
   }
 }
